285e92ebc05ddc281ba3f25b3bc58222e8878665
Steffgroe <Steffgroe@users.noreply.github.com>
2019-05-29 13:39:07 +0200

    place turret and planes

--- grid/test.py
+++ grid/test.py
@@ -3,7 +3,8 @@
 from tkinter import *
 from PIL import ImageTk, Image
 import math
-
+import random
+import numpy as np
 
 class MouseMover():
   
@@ -30,8 +31,16 @@ class MouseMover():
     canvas.move(self.item, xc-self.previous[0], yc-self.previous[1])
     self.previous = (xc, yc)
 
+class Model():
+	def __init__(self):
+		self.planes = {}
+		self.planesCounter = 0
+		self.defenceCounter = 0
+
+
+
+
 def getClosestTile(n):
-	print ("n",n)
 	n = math.floor(n)
 	return n
 
@@ -48,56 +57,124 @@ def loadTextures(canvas,cellwidth,cellheight):
 	canvas.flak = ImageTk.PhotoImage(canvas.flak.resize((cellwidth, cellheight), Image.ANTIALIAS))
 	canvas.land = Image.open("land.jpg")
 	canvas.land = ImageTk.PhotoImage(canvas.land.resize((cellwidth, cellheight), Image.ANTIALIAS))
+	canvas.airplane = Image.open("plane.jpg")
+	canvas.airplane = ImageTk.PhotoImage(canvas.airplane.resize((cellwidth,cellheight), Image.ANTIALIAS))
 
 def drawState(self):
 	w=self.winfo_width()
 	h=self.winfo_height()
 
-	cellwidth = w//10
+	cellwidth = w//10	
 	cellheight=h//10
-	if self.init == 1:
+	canvas.model.cellheight = cellheight
+	canvas.model.cellwidth = cellwidth
+	
+	if self.init ==1:
 		self.init = 0
+		
 		loadTextures(canvas,cellwidth,cellheight)
+		row = random.randint(0,5)
+		col = random.randint(0,5)
+		canvas.model.plane={
+		"name": "plane1",
+		"row": row,
+		"col": col,
+		"health": 100
+		}
+		canvas.create_image(col*cellwidth,row*cellheight,image=canvas.airplane,anchor=NW)
+		print("Plane initialized at:",row,col)
+
 		for row in range(10):
 			for col in range(10):
 				canvas.create_image(col*cellwidth,row*cellheight,image=canvas.land,anchor=NW)
+		
 	else:
+		drawPlanes(canvas)
+
+def createDefence(self):
+		w=self.winfo_width()
+		h=self.winfo_height()
+
+		cellwidth = w//10	
+		cellheight=h//10
 		row = getClosestTile(self.y/cellheight)
 		col = getClosestTile(self.x/cellwidth)	
-		# print("row",row,'col',col)
+		print("row,col",row,col)
 		canvas.create_image(col*cellwidth,row*cellheight,image=canvas.flak,anchor=NW)
 		create_circle((col+0.5)*cellwidth,(row+0.5)*cellheight, cellheight, canvas)
+
+		defence =	{
+		"name": canvas.model.defenceCounter,
+		"row": row,
+		"col": col,
+		"health": 100
+		}
+
+	
+		if canvas.model.defenceCounter ==0:
+			canvas.model.defences = [defence]
+		else:
+			canvas.model.defences.append(defence)
+		canvas.model.defenceCounter+=1
+ 
+def drawPlanes(self):
+	plane = canvas.model.plane
+	row = plane.get("row")
+	col = plane.get("col")
+	speed = 1
+	row = row+speed
+	canvas.model.plane["row"] = row
+	canvas.model.plane["col"] = col
+	cellwidth = canvas.model.cellwidth	
+	cellheight= canvas.model.cellheight
+	print("Plane: (row,col)",row,col)
+	
+	canvas.create_image(col*cellwidth,row*cellheight,image=canvas.airplane,anchor=NW)
+	row = row - speed
+	print("Land:(row,col)",row,col)
+	canvas.create_image(col*cellwidth,row*cellheight,image=canvas.land,anchor=NW)
+				
 		
 
 def buttonhandler(event):
-	if event.widget==thebutton:
-		canvas.init = 1
+	if event.widget==draw:
 		drawState(canvas)
-    # if event.widget==select:
-	else:
+
+	if event.widget==placeTurret:
+		createDefence(canvas)
+
+	if event.widget==erase:
 	    canvas.delete('all')
+	else:
+		drawState(canvas)
+
 window=Tk()
 
 canvas = Canvas(window, width=500,height=500)
 canvas.grid(row=0,column=0,columnspan=2)
+canvas.init = 1
 # Get an instance of the MouseMover object
 mm = MouseMover()
-
+canvas.model = Model()
 # Bind mouse events to methods (could also be in the constructor)
 canvas.bind("<Button-1>", mm.select)
 canvas.bind("<B1-Motion>", mm.drag)
-thebutton=Button(window,text="Draw")
-thebutton.grid(row=1,column=10)
+draw=Button(window,text="Draw")
+draw.grid(row=1,column=3)
+placeTurret=Button(window,text="Place turret")
+placeTurret.grid(row=1,column=4)
 select=Button(window, text="Select")
 select.grid(row=1,column =2)
-thebutton2=Button(window,text="Erase")
-thebutton2.grid(row=1,column=1)
+erase=Button(window,text="Erase")
+erase.grid(row=1,column=1)
+
 window.update_idletasks()
 canvas.x = mm.x
 canvas.y = mm.y
-thebutton.bind('<Button-1>',buttonhandler)
-thebutton2.bind('<Button-1>',buttonhandler)
+draw.bind('<Button-1>',buttonhandler)
+erase.bind('<Button-1>',buttonhandler)
 select.bind('<Button-1>',buttonhandler)
+placeTurret.bind('<Button-1>',buttonhandler)
 #drawState(canvas)
 window.mainloop()
     
